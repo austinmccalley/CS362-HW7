3ae4e573db8e2014ce4c54f737ecc9e3c4867f69
Austin McCalley <austin@austinmccalley.com>
2021-05-31 10:59:41 -0700

    Test case #2, buzz

--- question1/test_fizzbuzz.py
+++ question1/test_fizzbuzz.py
@@ -6,3 +6,7 @@ class test_FizzBuzz(unittest.TestCase):
     def testFizz(self):
         for i in [3**1, 3**2, 3**3, 3**4, 3**5, 3**6]:
           self.assertEqual(fizzBuzz(i), 'Fizz')
+
+    def testBuzz(self):
+      for i in [5**1, 5**2, 5**3, 5**4, 5**5]:
+        self.assertEqual(fizzBuzz(i), 'Buzz')

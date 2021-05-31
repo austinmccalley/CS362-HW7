ccc33c76653ff9be538d6d58dce26fa3d66c4614
Austin McCalley <austin@austinmccalley.com>
2021-05-31 11:01:09 -0700

    Test case #3, fizzbuzz

--- question1/test_fizzbuzz.py
+++ question1/test_fizzbuzz.py
@@ -5,8 +5,12 @@ from fizz import fizzBuzz
 class test_FizzBuzz(unittest.TestCase):
     def testFizz(self):
         for i in [3**1, 3**2, 3**3, 3**4, 3**5, 3**6]:
-          self.assertEqual(fizzBuzz(i), 'Fizz')
+            self.assertEqual(fizzBuzz(i), 'Fizz')
 
     def testBuzz(self):
-      for i in [5**1, 5**2, 5**3, 5**4, 5**5]:
-        self.assertEqual(fizzBuzz(i), 'Buzz')
+        for i in [5**1, 5**2, 5**3, 5**4, 5**5]:
+            self.assertEqual(fizzBuzz(i), 'Buzz')
+
+    def testFizzBuzz(self):
+        for i in [5*3 * 1, 5 * 3 * 2, 5 * 3 * 3, 5 * 3 * 4]:
+            self.assertEqual(fizzBuzz(i), 'FizzBuzz')
